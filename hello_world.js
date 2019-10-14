cfcc7d4b7ef7b42218a80ffe70a32daf424fa0d7
user <you@exampalex.com>
2019-10-14 10:35:53 +0100

    initial commit

---
+++ hello_world.js
@@ -0,0 +1,13 @@
+// console.log("Hello World!");
+
+// console.log(Boolean(null));
+// console.log(Boolean("Fish"));
+// console.log(Boolean(''));
+// console.log(Boolean(0));
+// console.log(Boolean(1));
+// console.log(Boolean(undefined));
+// console.log(Boolean(NaN));
+
+console.log(3 + "Hello");
+console.log("route" + 6 + 6);
+console.log(6 + 6 + "route");
